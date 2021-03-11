a4fae303c18c55ee8f4129baef746bf0f26818bd
Sandro <sandro.bjelogrlic@gmail.com>
2021-03-11 21:46:38 +0100

    added tests for boost.py

--- tests/test_boost.py
+++ tests/test_boost.py
@@ -1,14 +1,16 @@
 import numpy as np
 import pytest
 
-from tboost.utils import recompute_leaves, compute_probability
+from tboost.utils import TBoost
 
 
 @pytest.fixture
 def rec_leaf_values(X_y, leaves_indexes, model_params):
     """Fixture to recompute the leaf values."""
     X, y = X_y
-    remapped_leaves, leaf_mapping = recompute_leaves(leaves_indexes, X, y, model_params=model_params)
+    tb = TBoost(model_params=model_params)._fit(leaves_indexes, y)
+
+    remapped_leaves = tb._apply_leaf_map(leaves_indexes)
     return remapped_leaves
 
 
@@ -21,14 +23,14 @@ def test_probability_calc(rec_leaf_values, pred_proba):
     """Test the recalculation of the probabilities."""
     # test that the recomputed probability with no tree index corresponds to the probability
     # of the xgboost model
-    new_pred_proba = compute_probability(rec_leaf_values, tree_index=None)
+    new_pred_proba = TBoost.compute_probability(rec_leaf_values, tree_index=None)
     np.testing.assert_array_almost_equal(new_pred_proba, pred_proba[:, 3], decimal=3)
 
     # Test the probability for every index.
     for tree_index in range(4):
         # compute proba when tree_index=0 corresponds to the bias term
-        new_pred_proba = compute_probability(rec_leaf_values, tree_index=tree_index + 1)
+        new_pred_proba = TBoost.compute_probability(rec_leaf_values, tree_index=tree_index + 1)
         np.testing.assert_array_almost_equal(new_pred_proba, pred_proba[:, tree_index], decimal=3)
 
-    bias_term = np.unique(compute_probability(rec_leaf_values, tree_index=0))
+    bias_term = np.unique(TBoost.compute_probability(rec_leaf_values, tree_index=0))
     np.testing.assert_array_equal(bias_term, 0.5)
