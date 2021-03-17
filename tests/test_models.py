9f43490520c7115d5c78f31dd46f3a631a0c2bca
Sandro <sandro.bjelogrlic@gmail.com>
2021-03-17 19:29:39 +0100

    added test ofr LGBMclassifier:

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,8 @@
 import pytest
 import numpy as np
 import xgboost as xgb
-from tboost.models import XGBTransferLearner
+import lightgbm as lgb
+from tboost.models import XGBTransferLearner, LGBTransferLearner
 from sklearn.exceptions import NotFittedError
 
 
@@ -18,6 +19,32 @@ def test_tboost_vs_xgb(X_y) -> None:
     probas = model.predict_proba(X)
 
     tbooster = XGBTransferLearner(model)
+    tbooster.fit(X, y)
+
+    tboost_probas = tbooster.predict_proba(X)
+
+    np.testing.assert_array_almost_equal(probas, tboost_probas)
+
+
+def test_tboost_vs_lgb(X_y) -> None:
+    """Test that using bins=1 puts everything into 1 bucket."""
+    X, y = X_y
+
+    model = lgb.LGBMClassifier(
+        max_depth=2,
+        reg_lambda=0,
+        num_leaves=4,
+        n_estimators=4,
+        min_child_samples=0,  # default value here does not allow to fit multiple trees.
+    )
+
+    with pytest.raises(NotFittedError):
+        LGBTransferLearner(model)
+
+    model.fit(X, y)
+    probas = model.predict_proba(X)
+
+    tbooster = LGBTransferLearner(model)
     tbooster.fit(X, y)
 
     tboost_probas = tbooster.predict_proba(X)
